be2de5654e0ac43909844af641fc23331a1bb813
Ghassan Abdullah <gh1assan@gmil.com>
2025-08-26 10:09:40 +0300

    Fix S3 bucket configuration

--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -1,14 +1,7 @@
-# terraform/environments/prod/backend.tf
-# Create this file to explicitly configure the backend
-
 terraform {
   backend "s3" {
     bucket = "ghassan8080-wordpress-eks-project"
     key    = "wordpress-eks/terraform.tfstate"
     region = "us-west-2"
-    
-    # Optional: Enable state locking and consistency checking
-    # dynamodb_table = "terraform-state-lock"
-    # encrypt        = true
   }
 }
